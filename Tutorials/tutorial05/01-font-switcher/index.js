b47a7a0ca9d4fd9e3c89b63c28f4854643dca0cf
nahiykel <nathanhiykel2025@u.northwestern.edu>
2022-04-29 22:59:12 -0500

    Tutorial 5

--- Tutorials/tutorial05/01-font-switcher/index.js
+++ Tutorials/tutorial05/01-font-switcher/index.js
@@ -1,12 +1,21 @@
+let fontSize = 2.4;
+
 const makeBigger = () => {
-   alert('make bigger!');
+   fontSize += 0.2;
+   changeFont (); 
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   fontSize -= 0.2; 
+   changeFont ();
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+const changeFont = () => {
+   document.querySelector("h1").style.fontSize = `${fontSize + 0.2}em`;
+   document.querySelector(".content").style.fontSize = `${fontSize + 0.2}em`;
+}
+
+
+document.querySelector("#a1").addEventListener('click', makeBigger);
+document.querySelector("#a2").addEventListener('click', makeSmaller);
+
