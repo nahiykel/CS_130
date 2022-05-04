a1665dd5e4a2f9c6f4b583c4818dec69bf5e3257
nahiykel <nathanhiykel2025@u.northwestern.edu>
2022-05-04 17:31:08 -0500

    Commited?

---
+++ Lectures/lecture12/00-warmup/main.js
@@ -0,0 +1,29 @@
+
+// const square = '<rect x="291" y="70" width="40" height="40" stroke="black" stroke-width="3" fill="pink"></rect>'
+// const triangle = ' <polygon points="100,100 150,100 125,135" stroke="black" stroke-width="3" fill="teal"></polygon>';
+
+
+// document.querySelector('svg').insertAdjacentHTML("beforeend", square);
+
+// document.querySelector('svg').insertAdjacentHTML("beforeend", triangle);
+
+// Step 1: What event do we want to use to create shapes:
+
+// Step 2: Create an event handler to attacch to the event: 
+
+const addShape = (ev) => {
+    console.log(ev.offsetX, ev.offsetY);
+    console.log('Add shape');
+    let color = document.querySelector('#color').value;
+    const circle= `
+        <circle cx="${ev.offsetX}" cy="${ev.offsetY}" r="20" stroke="black" stroke-width="3" fill="red"></circle>' `;
+    document.querySelector('svg').insertAdjacentHTML("beforeend", circle);
+    let size = document.querySelector('#size').value; 
+};
+
+
+/**
+ * Your job: when the user clicks the svg element, 
+ * draw the shape the corresponds with the controls 
+ * in the left-hand panel.
+ */
